b6768f463a5a3b1b572011d4915a52db3ec7e06e
zhanss <1579255101@qq.com>
2019-11-25 00:05:06 +0800

    环形链表

--- data-structure/src/main/java/cn/com/zhanss/datastructure/linkedlist/CircleLinkedListTest.java
+++ data-structure/src/main/java/cn/com/zhanss/datastructure/linkedlist/CircleLinkedListTest.java
@@ -14,9 +14,11 @@ public class CircleLinkedListTest {
 
     @Test
     public void testCircleLinkedList() {
-        doCircle(3);
+        doCircle(5);
         // 遍历下
         showBoy ();
+        // 开始出圈，顺序：2->4->1->5->3
+        countBoy(3, 2);
     }
 
     private Boy first = null;
@@ -47,13 +49,49 @@ public class CircleLinkedListTest {
         }
         Boy curBoy = first;
         while (true) {
-            System.out.printf("出列的Boy标号：%d", curBoy.no);
+            System.out.printf("出列的Boy标号：%d\n", curBoy.no);
             if (curBoy.next == first) {
                 System.out.println("结束了！");
                 return;
             }
             curBoy = curBoy.next;
         }
+    }
+
+    /**
+     * 根据用户的输入，获得小孩出圈的顺序
+     * @param startNo
+     *        从哪开始
+     * @param countNum
+     *        数到几出圈
+     */
+    public void countBoy (int startNo, int countNum) {
+        if (first == null || startNo < 1 || countNum < 1) {
+            return;
+        }
+        Boy helper = first;
+        int i = 0;
+        // help始终保持在最后一个节点（头节点前面）
+        while (helper.next != first) {
+            // 从startNo开始报数
+            if (i < startNo - 1) {
+                first = first.next;
+            }
+            helper = helper.next;
+            i ++;
+        }
+        while (helper != first) {
+            // 同时移动countNum（数到countNum出圈）
+            for (int j = 0; j < countNum - 1; j++) {
+                first = first.next;
+                helper = helper.next;
+            }
+            // 这时first指向的节点出圈
+            System.out.printf("小孩%d出圈\n", first.no);
+            first = first.next;
+            helper.next = first;
+        }
+        System.out.printf("最后一个节点%d出圈", first.no);
     }
 }
 
