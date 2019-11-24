0f36f290f564746464284976719e89eb2d35d261
zhanss <1579255101@qq.com>
2019-11-24 22:33:55 +0800

    环形链表

--- data-structure/src/main/java/cn/com/zhanss/datastructure/linkedlist/CircleLinkedListTest.java
+++ data-structure/src/main/java/cn/com/zhanss/datastructure/linkedlist/CircleLinkedListTest.java
@@ -1,5 +1,7 @@
 package cn.com.zhanss.datastructure.linkedlist;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.Test;
 
 /**
@@ -12,19 +14,50 @@ public class CircleLinkedListTest {
 
     @Test
     public void testCircleLinkedList() {
-
+        doCircle(3);
+        // 遍历下
+        showBoy ();
     }
+
+    private Boy first = null;
 
     public void doCircle(int num) {
         if (num < 1) {
             return;
         }
+        Boy curBoy = null;
         for (int i = 1; i <= num; i ++) {
+            Boy boy = new Boy(i, i+ "", null);
+            if (i == 1) {
+                first = boy;
+                // 构成环
+                first.next = first;
+                curBoy = first;
+            } else {
+                curBoy.next = boy;
+                boy.next = first;
+                curBoy = boy;
+            }
+        }
+    }
 
+    public void showBoy () {
+        if (first == null) {
+            return;
+        }
+        Boy curBoy = first;
+        while (true) {
+            System.out.printf("出列的Boy标号：%d", curBoy.no);
+            if (curBoy.next == first) {
+                System.out.println("结束了！");
+                return;
+            }
+            curBoy = curBoy.next;
         }
     }
 }
 
+@AllArgsConstructor
 class Boy {
     public Integer no;
 
