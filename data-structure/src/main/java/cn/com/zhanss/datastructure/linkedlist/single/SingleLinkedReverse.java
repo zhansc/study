20d73e28fa160532f9b2e0c168ce30dda50586ed
zhanss <1579255101@qq.com>
2022-05-02 17:30:03 +0800

    单链表反转

--- data-structure/src/main/java/cn/com/zhanss/datastructure/linkedlist/single/SingleLinkedReverse.java
+++ data-structure/src/main/java/cn/com/zhanss/datastructure/linkedlist/single/SingleLinkedReverse.java
@@ -1,5 +1,6 @@
 package cn.com.zhanss.datastructure.linkedlist.single;
 
+import lombok.Data;
 import org.junit.Test;
 
 /**
@@ -29,6 +30,10 @@ public class SingleLinkedReverse {
         System.out.println("队列头--->"+ singleLinked.headPeek());
 
         System.out.println("队列--->"+ head);
+
+        Node<Integer> randomTree = randomTree(20, 5);
+        Node reverseV2 = reverseV2(randomTree);
+        System.out.println("reverseV2--->"+ reverseV2);
     }
 
     /**
@@ -72,6 +77,91 @@ public class SingleLinkedReverse {
             pre = cur;
             head = cur;
             cur = next;
+        }
+        return head;
+    }
+
+    /**
+     * 构建一个随机长度节点随机大小的单链表
+     * @param maxValue
+     * @param maxLength
+     * @return
+     */
+    public Node randomTree(int maxValue, int maxLength) {
+        if (maxLength < 1) {
+            return null;
+        }
+        int treeSize = (int) (Math.random() * maxLength);
+        Node<Integer> head = new Node<>((int) (Math.random() * maxValue));
+        buildNode(head, maxValue, treeSize);
+        return head;
+    }
+
+    private void buildNode(Node head, int maxValue, int treeSize) {
+        if (treeSize > 0) {
+            Node<Integer> next = new Node<>((int) (Math.random() * maxValue));
+            head.next = next;
+            next.pre = head;
+            buildNode(next, maxValue, -- treeSize);
+        }
+    }
+
+    @Data
+    private class Node<T> {
+        private T value;
+
+        private Node pre;
+
+        private Node next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    public Node reverseV2(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node pre = null;
+        Node cur = head;
+        Node next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            cur.pre = next;
+            pre = head;
+            cur = next;
+            if (cur != null) {
+                head = cur;
+            }
+        }
+        return head;
+    }
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = head;
+            cur = next;
+            if (cur != null) {
+                head = cur;
+            }
         }
         return head;
     }
