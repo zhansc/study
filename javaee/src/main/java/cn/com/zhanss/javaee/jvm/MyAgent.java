4a4f4852856d78f9a87cc74c3f5693ea41e778a3
zhanss <1579255101@qq.com>
2022-11-03 11:07:15 +0800

    javaagent测试

--- javaee/src/main/java/cn/com/zhanss/javaee/jvm/MyAgent.java
+++ javaee/src/main/java/cn/com/zhanss/javaee/jvm/MyAgent.java
@@ -9,6 +9,20 @@ import java.lang.instrument.Instrumentation;
  * @since 2022-10-18
  */
 public class MyAgent {
+
+    /**
+     * 动态代理一种方式
+     * 1、将当前类打包
+     * 2、修改MyAgent的Edit Configurations，增加VM Options配置
+     * -javaagent:E:\youzan\IdeaProjects\study\javaee\target\javaee-0.0.1-SNAPSHOT.jar=testjavaagent
+     * -javaagent:打包的绝对路径=premain方法的第一个参数
+     * 3、执行main方法
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hello javaAgent");
+    }
+
     /**
      * @param agentArgs 是premain 函数得到的程序参数，随同 “–javaagent”一起传入
      * @param inst 一个java.lang.instrument.Instrumentation 的实例，由 JVM 自动传入
@@ -28,3 +42,4 @@ public class MyAgent {
         System.out.println(agentArgs);
     }
 }
+
