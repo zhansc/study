b074e02525253fab57f89e4ceef4d6cec6adbe91
zhansc <1579255101@qq.com>
2023-04-25 20:18:56 +0800

    利用前缀树接替

--- data-structure/src/main/java/cn/com/zhanss/datastructure/array/ArrayExercise.java
+++ data-structure/src/main/java/cn/com/zhanss/datastructure/array/ArrayExercise.java
@@ -2,6 +2,9 @@ package cn.com.zhanss.datastructure.array;
 
 import cn.com.zhanss.datastructure.doexercise.random.Rand2Rand;
 import cn.com.zhanss.datastructure.heap.MyHeap;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.Test;
 
 import java.util.*;
@@ -419,4 +422,89 @@ public class ArrayExercise {
         return matrix;
     }
 
+    /**
+        给你一个长度为 n 的数组 words ，该数组由 非空 字符串组成。
+        定义字符串 word 的 分数 等于以 word 作为 前缀 的 words[i] 的数目。
+        例如，如果 words = ["a", "ab", "abc", "cab"] ，那么 "ab" 的分数是 2 ，因为 "ab" 是 "ab" 和 "abc" 的一个前缀。
+        返回一个长度为 n 的数组 answer ，其中 answer[i] 是 words[i] 的每个非空前缀的分数 总和 。
+        注意：字符串视作它自身的一个前缀。
+
+        输入：words = ["abc","ab","bc","b"]
+        输出：[5,4,3,2]
+        解释：对应每个字符串的答案如下：
+        - "abc" 有 3 个前缀："a"、"ab" 和 "abc" 。
+        - 2 个字符串的前缀为 "a" ，2 个字符串的前缀为 "ab" ，1 个字符串的前缀为 "abc" 。
+        总计 answer[0] = 2 + 2 + 1 = 5 。
+        - "ab" 有 2 个前缀："a" 和 "ab" 。
+        - 2 个字符串的前缀为 "a" ，2 个字符串的前缀为 "ab" 。
+        总计 answer[1] = 2 + 2 = 4 。
+        - "bc" 有 2 个前缀："b" 和 "bc" 。
+        - 2 个字符串的前缀为 "b" ，1 个字符串的前缀为 "bc" 。
+        总计 answer[2] = 2 + 1 = 3 。
+        - "b" 有 1 个前缀："b"。
+        - 2 个字符串的前缀为 "b" 。
+        总计 answer[3] = 2 。
+
+        输入：words = ["abcd"]
+        输出：[4]
+        解释：
+        "abcd" 有 4 个前缀 "a"、"ab"、"abc" 和 "abcd"。
+        每个前缀的分数都是 1 ，总计 answer[0] = 1 + 1 + 1 + 1 = 4 。
+
+        链接：https://leetcode.cn/problems/sum-of-prefix-scores-of-strings
+     */
+    @Test
+    public void test() {
+        String[] words = new String[]{"abcd"};
+        int[] scores = new Solution().sumPrefixScores(words);
+        Arrays.stream(scores).forEach(item -> System.out.print(" "+ item));
+        System.out.println();
+    }
+
+    /**
+     * 前缀树
+     */
+    class Solution {
+        @Data
+        @Getter
+        @Setter
+        class PrefixTree {
+            int pass;
+            int end;
+            Character ch;
+            PrefixTree next;
+        }
+        public int[] sumPrefixScores(String[] words) {
+            int length = words.length;
+            if (words.length == 0) {
+                return null;
+            }
+            int i = 0;
+            int[] answers = new int[length];
+            Map<String, Integer> cache = new HashMap<>();
+            Map<String, Integer> subCache = new HashMap<>();
+            PrefixTree prefixTree = new PrefixTree();
+            for (String word : words) {
+                answers[i ++] = process(prefixTree, words, word);
+            }
+            return answers;
+        }
+        private int process(PrefixTree prefixTree, String[] words, String word) {
+            if (word == null || word.length() == 0) {
+                return 0;
+            }
+            for (Character ch : word.toCharArray()) {
+                if (prefixTree.ch.equals(ch)) {
+                    prefixTree.pass ++;
+                } else {
+                    PrefixTree next = new PrefixTree();
+                    next.ch = ch;
+                    next.pass ++;
+                    prefixTree.next = next;
+                }
+            }
+            prefixTree.end ++;
+            return 0;
+        }
+    }
 }
